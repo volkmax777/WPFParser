541903f78d2f83d6c5428d8612e84408b6aa7947
volkmax777 <maksim.vo2012@gmail.com>
2020-04-26 06:38:01 +0500

    start

---
+++ Wpfparser/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpfparser
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Link.GotFocus += Link_GotFocus;
+        }
+
+        private void Link_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (Clipboard.GetText().Contains("http"))
+            {
+                Link.Text = Clipboard.GetText();
+            }
+            else if (Link.Text.Equals("Введите ссылку")) Link.Text = "";
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (Universal.IsChecked == true)
+            {
+                var hg = Parser.UniPars(Link.Text);
+                Header.Text = hg[0];
+                Content.Text = hg[1];
+            }
+            if (Lenta.IsChecked == true)
+            {
+                if (Link.Text.Contains("lenta.ru"))
+                {
+                    var hg = Parser.LentaPars(Link.Text);
+                    Header.Text = hg[0];
+                    Content.Text = hg[1];
+                }
+                else Header.Text = "Введи ссылку lenta.ru";
+            }
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Wpfparser/Parser.cs
@@ -0,0 +1,121 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Text;
+using System.Net;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Wpfparser
+{
+    public class Parser
+    {
+        public static string[] LentaPars(string args)
+        {
+            try
+            {
+                var txtHTML = GetPage(args);
+                var doc = new HtmlDocument();
+                doc.LoadHtml(txtHTML);
+                HtmlNode node = doc.DocumentNode.SelectSingleNode("//*/div[@class='b-text clearfix js-topic__text']");
+                var txtTitle = FindText(txtHTML, @"<title>", @"</title>");
+                var content = node.InnerText;
+                return new string[] { txtTitle, content };
+            }
+            catch
+            {
+                return new string[] {"Что-то пошло не так", ""};
+            }
+        }
+        public static string[] UniPars(string args)
+        {
+            try
+            {
+                var txtHTML = GetPage(args);
+                var doc = new HtmlDocument();
+                doc.LoadHtml(txtHTML);
+                var txtTitle = FindText(txtHTML, @"<title>", @"</title>");
+                var txtP = FindText(txtHTML, @"<p>", @"</p>");
+                var content = RemoveUnwantedTags(txtP);
+                return new string[] { txtTitle, content };
+            }
+            catch
+            {
+                return new string[] { "Что-то пошло не так", ""};
+            }
+        }
+        public static string RemoveUnwantedTags(string data)
+        {
+            if (string.IsNullOrEmpty(data)) return string.Empty;
+
+            var document = new HtmlDocument();
+            document.LoadHtml(data);
+
+            var acceptableTags = new String[] { "strong", "em", "u" };
+
+            var nodes = new Queue<HtmlNode>(document.DocumentNode.SelectNodes("./*|./text()"));
+            while (nodes.Count > 0)
+            {
+                var node = nodes.Dequeue();
+                var parentNode = node.ParentNode;
+
+                if (!acceptableTags.Contains(node.Name) && node.Name != "#text")
+                {
+                    var childNodes = node.SelectNodes("./*|./text()");
+
+                    if (childNodes != null)
+                    {
+                        foreach (var child in childNodes)
+                        {
+                            nodes.Enqueue(child);
+                            parentNode.InsertBefore(child, node);
+                        }
+                    }
+
+                    parentNode.RemoveChild(node);
+
+                }
+            }
+
+            return document.DocumentNode.InnerHtml;
+        }
+        public static String FindText(string source, string prefix, string suffix)
+        {
+            var prefixPosition = source.IndexOf(prefix, StringComparison.OrdinalIgnoreCase);
+            var suffixPosition = source.IndexOf(suffix, prefixPosition + prefix.Length, StringComparison.OrdinalIgnoreCase);
+            if ((prefixPosition >= 0) && (suffixPosition >= 0) && (suffixPosition > prefixPosition) && ((prefixPosition + prefix.Length) <= suffixPosition))
+            {
+                return source.Substring(prefixPosition + prefix.Length, suffixPosition - prefixPosition - prefix.Length);
+            }
+            else
+            {
+                return String.Empty;
+            }
+        }
+        public static string GetPage(string url)
+        {
+            var result = String.Empty;
+            var request = (HttpWebRequest)WebRequest.Create(url);
+            var response = (HttpWebResponse)request.GetResponse();
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                var responseStream = response.GetResponseStream();
+                if (responseStream != null)
+                {
+                    StreamReader streamReader;
+                    if (response.CharacterSet != null)
+                    {
+                        Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+                        streamReader = new StreamReader(responseStream, Encoding.GetEncoding(response.CharacterSet));
+                    }
+                    else
+                        streamReader = new StreamReader(responseStream);
+                    result = streamReader.ReadToEnd();
+                    streamReader.Close();
+                }
+                response.Close();
+            }
+            return result;
+        }
+    }
+}
